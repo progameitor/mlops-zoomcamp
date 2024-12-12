a16f0d5883fb32a44150baadaed817ac9215ef78
progameitor <55921129+progameitor@users.noreply.github.com>
2024-12-12 08:41:29 +0000

    much code added

---
+++ 06-best-practices/code/tests/model_test.py
@@ -0,0 +1,21 @@
+import lambda_function
+#
+def test_prepare_features():
+
+    model_service = model.ModelService(None)
+    
+    ride = {
+        "PULocationID": 130,
+        "DOLocationID": 205,
+        "trip_distance": 3.66,
+        
+    }
+    
+    actual_features = model_service.prepare_features()
+    
+    expected_features = {
+        "PU_DO": "130_205",
+        "trip_distance": 3.66        
+    }
+    
+    assert actual_features == expected_features
